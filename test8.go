1640accf3cb93f0be65fe290dcc0766f3e7e27d9
Zippo-Wang <852420284@qq.com>
2024-04-17 14:48:46 +0800

    feat(test8): test8

---
+++ test8.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	test8()
+}
+
+func test8() {
+	fmt.Println("1")
+	fmt.Println("2")
+	fmt.Println("3")
+	fmt.Println("44")
+	fmt.Println("55")
+	fmt.Println("6")
+	fmt.Println("7")
+}
