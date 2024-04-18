8274051a046682f5c83cd2cae8e53ed436d7f488
wangzepeng <852420284@qq.com>
2024-04-18 11:16:47 +0800

    feat(test6): test6 (#22)

--- test7.go
+++ test7.go
@@ -1,10 +1,23 @@
 package main
 
-func main() {
-	test7()
-	test7()
+import (
+	"fmt"
+)
+
+func funUsePointer(paramSlice *[]int) {
+	paramSliceV := *paramSlice
+	paramSliceV[2] = 9
+	for index := 0; index < 6; index++ {
+		*paramSlice = append(*paramSlice, index)
+	}
+	fmt.Printf("funUsePointer paramSlice =%v\n", paramSlice)
 }
 
-func test7() {
-
+func main() {
+	slice := make([]int, 5)
+	slice[0] = 2
+	slice[1] = 3
+	fmt.Println(slice)
+	funUsePointer(&slice)
+	fmt.Println(slice)
 }
